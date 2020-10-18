4790f34e25717ad3f4fa566f200c805ec289a948
vip32 <mini32@gmail.com>
2020-10-18 17:33:34 +0200

    build cc

---
+++ build/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Readability", "RCS1018:Add accessibility modifiers.", Justification = "<Pending>")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+[assembly: SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "<Pending>")]
+[assembly: SuppressMessage("Style", "IDE0009:Member access should be qualified.", Justification = "<Pending>")]
+[assembly: SuppressMessage("Simplification", "RCS1214:Unnecessary interpolated string.", Justification = "<Pending>")]
