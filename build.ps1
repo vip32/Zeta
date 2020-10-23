1a8b4fc8ab595fe6fc57ba37bfbab3e3cc6dc237
vip32 <mini32@gmail.com>
2020-10-23 17:38:58 +0200

    cc

--- build.ps1
+++ build.ps1
@@ -18,7 +18,7 @@ $TempDirectory = "$PSScriptRoot\\.tmp"
 
 $DotNetGlobalFile = "$PSScriptRoot\\global.json"
 $DotNetInstallUrl = "https://dot.net/v1/dotnet-install.ps1"
-$DotNetChannel = "Current" # https://docs.microsoft.com/de-de/dotnet/core/tools/dotnet-install-script
+$DotNetChannel = "Current"
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
