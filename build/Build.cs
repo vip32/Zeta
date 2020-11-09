5e8e78d6ec201eae8d660831541691de897c1d9c
vp <vincent.van.proosdij@bridging-it.de>
2020-11-09 20:26:55 +0100

    fix2

--- build/Build.cs
+++ build/Build.cs
@@ -41,7 +41,7 @@ class Build : NukeBuild
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion] readonly GitVersion GitVersion;
+    [GitVersion(Framework="net5.0")] readonly GitVersion GitVersion;
 
     AbsolutePath SourceDirectory => RootDirectory / "src"; // TODO
 
